7137c4d82c9bc81155684a333feb8c9423073c6b
Sterling Hanenkamp <sterling@hanenkamp.com>
2017-07-02 22:39:39 -0500

    Customizing for my own prefs

--- setup_terraform.sh
+++ setup_terraform.sh
@@ -2,8 +2,10 @@
 # Usage:
 #	. ./setup_terraform.sh
 
-export TF_VAR_number_of_workers=3
-export TF_VAR_do_token=$(cat ./secrets/DO_TOKEN)
+export TF_VAR_number_of_workers=2
+export TF_VAR_do_token=$(zostay-get-secret DIGITAL_OCEAN_TOKEN)
+export TF_VAR_prefix=phoenix-
+export TF_VAR_ssh_private_key="~/.ssh/id_rsa_do_k8s"
 
 function get_ssh_version {
     # ssh -V prints to stderr, redirect
@@ -15,7 +17,7 @@ function get_ssh_version {
 
 # if ssh version is under 6.9, use -lf, otherwise must use the -E version
 if ! awk -v ver="$(get_ssh_version)" 'BEGIN { if (ver < 6.9) exit 1; }'; then
-    export TF_VAR_ssh_fingerprint=$(ssh-keygen -lf ~/.ssh/id_rsa.pub | awk '{print $2}')
+    export TF_VAR_ssh_fingerprint=$(ssh-keygen -lf ~/.ssh/id_rsa_do_k8s.pub | awk '{print $2}')
 else
-    export TF_VAR_ssh_fingerprint=$(ssh-keygen -E MD5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | sed 's/MD5://g')
+    export TF_VAR_ssh_fingerprint=$(ssh-keygen -E MD5 -lf ~/.ssh/id_rsa_do_k8s.pub | awk '{print $2}' | sed 's/MD5://g')
 fi
